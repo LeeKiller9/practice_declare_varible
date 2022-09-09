3b98b94e8a988d84dc44d3e0427c6bccd7efaec9
LeeKiller9 <112848883+LeeKiller9@users.noreply.github.com>
2022-09-09 11:18:55 +0700

    add index, style, jv files

---
+++ README.md
@@ -0,0 +1,1 @@
+#Declare and useing varible
---
+++ javascript.js
@@ -0,0 +1,38 @@
+let i = 10
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+
+function show_variable() {
+    document.write('i = ' + i)
+    document.write('<br/>')
+    document.write('f = ' + f)
+    document.write('<br/>')
+    document.write('b = ' + b)
+    document.write('<br/>')
+    document.write('s = ' + s)
+}
+
+let width = 20
+let height = 10
+let area = width * height
+
+function show_area() {
+    document.write('width = ' + width)
+    document.write('<br/>')
+    document.write('height = ' + height)
+    document.write('<br/>')
+    document.write('Area = ' + area)
+}
+
+function check_number() {
+    let a = prompt("Input first number")
+    let c = prompt("Input second number")
+    let d = a % c
+    if (d == 0) {
+        alert("Reminder is " + d + " => " +  a + " is multiple of " + c)
+    } else {
+        alert("Reminder is " + d + " => " +  a + " is not multiple of " + c)
+    }
+}
+
